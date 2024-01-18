582e1d6afdffcb0a3ecbfdf58d6f505f7ce67307
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-01-18 14:41:19 +0300

    Add Topbar and Filters components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Navbar from "./components/NavBar.vue";
+import Topbar from "./components/TopBar.vue";
+import Filters from "./components/Filters.vue";
 </script>
 
 <template>
-  <div class="bg-black overflow-hidden">
+  <div class="bg-black overflow-hidden max-h-screen">
+    <Topbar />
+    <Filters />
     <Navbar />
   </div>
 </template>
