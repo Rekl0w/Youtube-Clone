0bea74970d1a663fae3de7818248c46edf9bac59
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-01-18 14:41:00 +0300

    Add Filters component for user interface

---
+++ src/components/Filters.vue
@@ -0,0 +1,77 @@
+<script setup></script>
+
+<template>
+  <div class="bg-black overflow-hidden flex items-center w-full justify-start mt-6">
+    <div class="flex justify-center items-center bg-white text-black font-semibold rounded-xl min-w-fit p-4 h-8 ml-72 mb-3">
+        <button>
+            <p>All</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Music</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Games</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Mix</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Live</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Action-adventure games</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Sports Video Games</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Football</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Cooking</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Last Uploads</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>Watched</p>
+        </button>
+    </div>
+    <div class=" filter-btn flex justify-center items-center text-white font-semibold rounded-xl min-w-fit p-4 h-8 ml-4 mb-3">
+        <button>
+            <p>New Suggestions</p>
+        </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.filter-btn {
+    background-color: #272727;
+}
+
+.filter-btn:hover {
+    background-color: #3F3E3E;
+}
+</style>
